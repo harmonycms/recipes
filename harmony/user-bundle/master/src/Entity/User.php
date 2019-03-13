e6b3faf4895c0d1fabde49667342dde9a5c96061
David Sanchez <david38sanchez@gmail.com>
2019-03-13 00:22:51 -0400

    Add recipe for `Harmony/UserBundle`

---
+++ harmony/user-bundle/master/src/Entity/User.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Harmony\UserBundle\Model\ExtendedUserTrait;
+use Harmony\UserBundle\Security\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="user")
+ * @UniqueEntity(fields="email", message="Email already registered.")
+ * @UniqueEntity(fields="username", message="Username already registered.")
+ */
+class User implements UserInterface, \Serializable
+{
+
+    use ExtendedUserTrait;
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank()
+     */
+    private $fullName;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setFullName(string $fullName): void
+    {
+        $this->fullName = $fullName;
+    }
+
+    public function getFullName(): ?string
+    {
+        return $this->fullName;
+    }
+}

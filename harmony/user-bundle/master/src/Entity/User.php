4045171cd946bca99622112b2aa2fe421568f572
David Sanchez <david.sanchez@nventive.com>
2019-03-15 13:20:58 -0400

    Updating recipe after changing namespace for bundle

--- harmony/user-bundle/master/src/Entity/User.php
+++ harmony/user-bundle/master/src/Entity/User.php
@@ -5,7 +5,6 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Harmony\UserBundle\Model\User as BaseUser;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity()
@@ -24,32 +23,10 @@ class User extends BaseUser
     protected $id;
 
     /**
-     * @ORM\Column(type="string")
-     * @Assert\NotBlank()
-     */
-    protected $fullName;
-
-    /**
      * @return int
      */
     public function getId(): int
     {
         return $this->id;
-    }
-
-    /**
-     * @param string $fullName
-     */
-    public function setFullName(string $fullName): void
-    {
-        $this->fullName = $fullName;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getFullName(): ?string
-    {
-        return $this->fullName;
     }
 }

afbbca55ecec1f04a4bd8bacb107a68e8ecb6626
David Sanchez <david38sanchez@gmail.com>
2019-03-17 11:14:56 -0400

    Add User document class for MongoDB

---
+++ harmony/user-bundle/master/src/Document/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+use Harmony\Bundle\UserBundle\Model\User as BaseUser;
+
+/**
+ * @MongoDB\Document
+ */
+class User extends BaseUser
+{
+
+    /**
+     * @MongoDB\Id(strategy="auto")
+     */
+    protected $id;
+
+    /**
+     * Get the value of id.
+     *
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+}

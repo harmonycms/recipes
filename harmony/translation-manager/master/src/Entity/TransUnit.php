7c5f09606bab41e9a335ac4174e5c1c39b6015b8
David Sanchez <david.sanchez@nventive.com>
2019-03-28 11:16:54 -0400

    Update mapping ORM

--- harmony/translation-manager/master/src/Entity/TransUnit.php
+++ harmony/translation-manager/master/src/Entity/TransUnit.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use DateTime;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Harmony\Extension\TranslationManager\Manager\TransUnitInterface;
 use Harmony\Extension\TranslationManager\Model\Translation;
@@ -9,7 +11,14 @@ use Harmony\Extension\TranslationManager\Model\TransUnit as TransUnitModel;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
- * @ORM\Entity()
+ * @ORM\Entity(repositoryClass="Harmony\Extension\TranslationManager\Entity\TransUnitRepository")
+ * @ORM\Table(
+ *     name="translation_manager_trans_unit",
+ *     uniqueConstraints={
+ *          @ORM\UniqueConstraint(name="key_domain_idx", columns={"key_name", "domain"})
+ *     }
+ * )
+ * @ORM\HasLifecycleCallbacks()
  * @UniqueEntity(fields={"key", "domain"})
  * @author Cédric Girard <c.girard@lexik.fr>
  */
@@ -18,11 +27,17 @@ class TransUnit extends TransUnitModel implements TransUnitInterface
 
     /**
      * @ORM\Id
-     * @ORM\GeneratedValue
+     * @ORM\GeneratedValue(strategy="IDENTITY")
      * @ORM\Column(type="integer")
-     * @var int
+     * @var int $id
      */
     protected $id;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Translation", mappedBy="transUnit", cascade={"all"})
+     * @var Collection $translations
+     */
+    protected $translations;
 
     /**
      * Get id
@@ -47,19 +62,21 @@ class TransUnit extends TransUnitModel implements TransUnitInterface
     }
 
     /**
-     * {@inheritdoc}
+     * @ORM\PrePersist()
+     * @throws \Exception
      */
     public function prePersist()
     {
-        $this->createdAt = new \DateTime("now");
-        $this->updatedAt = new \DateTime("now");
+        $this->createdAt = new DateTime("now");
+        $this->updatedAt = new DateTime("now");
     }
 
     /**
-     * {@inheritdoc}
+     * @ORM\PreUpdate()
+     * @throws \Exception
      */
     public function preUpdate()
     {
-        $this->updatedAt = new \DateTime("now");
+        $this->updatedAt = new DateTime("now");
     }
 }

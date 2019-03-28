46ce07bf1190f1b6d397fc848c252f0ec0d16eaa
David Sanchez <david.sanchez@nventive.com>
2019-03-28 11:20:05 -0400

    Update mapping ORM

--- harmony/translation-manager/master/src/Entity/File.php
+++ harmony/translation-manager/master/src/Entity/File.php
@@ -2,14 +2,23 @@
 
 namespace App\Entity;
 
+use DateTime;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Harmony\Extension\TranslationManager\Manager\FileInterface;
 use Harmony\Extension\TranslationManager\Model\File as FileModel;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
- * @ORM\Entity()
+ * @ORM\Entity(repositoryClass="Harmony\Extension\TranslationManager\Entity\FileRepository")
+ * @ORM\Table(
+ *     name="translation_manager_file",
+ *     uniqueConstraints={
+ *          @ORM\UniqueConstraint(name="hash_idx", columns={"hash"})
+ *     }
+ * )
  * @UniqueEntity(fields={"hash"})
+ * @ORM\HasLifecycleCallbacks()
  * @author Cédric Girard <c.girard@lexik.fr>
  */
 class File extends FileModel implements FileInterface
@@ -17,11 +26,17 @@ class File extends FileModel implements FileInterface
 
     /**
      * @ORM\Id
-     * @ORM\GeneratedValue
+     * @ORM\GeneratedValue(strategy="IDENTITY")
      * @ORM\Column(type="integer")
      * @var int
      */
     protected $id;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Translation", mappedBy="file", cascade={"persist"})
+     * @var Collection $translations
+     */
+    protected $translations;
 
     /**
      * Get id
@@ -34,19 +49,21 @@ class File extends FileModel implements FileInterface
     }
 
     /**
-     * {@inheritdoc}
+     * @ORM\PrePersist()
+     * @throws \Exception
      */
     public function prePersist()
     {
-        $this->createdAt = new \DateTime("now");
-        $this->updatedAt = new \DateTime("now");
+        $this->createdAt = new DateTime("now");
+        $this->updatedAt = new DateTime("now");
     }
 
     /**
-     * {@inheritdoc}
+     * @ORM\PreUpdate()
+     * @throws \Exception
      */
     public function preUpdate()
     {
-        $this->updatedAt = new \DateTime("now");
+        $this->updatedAt = new DateTime("now");
     }
 }

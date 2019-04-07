3ab80cac831816e47643acf2fb75c73d1ad7f925
David Sanchez <david38sanchez@gmail.com>
2019-04-07 19:19:35 -0400

    Adding missing MongoDB config file + Document class

---
+++ harmony/static-page/master/src/Document/StaticPage.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Document;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+use Exception;
+use Harmony\Extension\StaticPage\Model\StaticPage as BaseStaticPage;
+use Symfony\Cmf\Component\Routing\RouteObjectInterface;
+
+/**
+ * Class StaticPage
+ * @MongoDB\Document()
+ *
+ * @package App\Document
+ */
+class StaticPage extends BaseStaticPage
+{
+
+    /**
+     * @MongoDB\Id(strategy="auto")
+     */
+    protected $id;
+
+    /**
+     * @var RouteObjectInterface[]|ArrayCollection
+     * @MongoDB\ReferenceMany(targetDocument="Harmony\Bundle\RoutingBundle\Model\Route", cascade={"persist", "remove"})
+     */
+    protected $routes;
+
+    /**
+     * StaticPage constructor.
+     *
+     * @throws Exception
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->routes = new ArrayCollection();
+    }
+
+    /**
+     * @return int|string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}

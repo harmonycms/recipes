21b5610b6b78873be171e91aba8a263d9d894ec1
David Sanchez <david.sanchez@nventive.com>
2019-05-03 14:30:32 -0400

    Add `group` mapping

--- harmony/user-bundle/master/src/Document/User.php
+++ harmony/user-bundle/master/src/Document/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Document;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
 use Harmony\Bundle\UserBundle\Model\User as BaseUser;
 
@@ -17,6 +18,19 @@ class User extends BaseUser
     protected $id;
 
     /**
+     * @MongoDB\ReferenceMany(targetDocument="AppBundle\Document\Group")
+     */
+    protected $groups;
+
+    /**
+     * User constructor.
+     */
+    public function __construct()
+    {
+        $this->groups = new ArrayCollection();
+    }
+
+    /**
      * Get the value of id.
      *
      * @return int|string
@@ -24,5 +38,29 @@ class User extends BaseUser
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Get Groups
+     *
+     * @return mixed
+     */
+    public function getGroups()
+    {
+        return $this->groups;
+    }
+
+    /**
+     * Set Groups
+     *
+     * @param mixed $groups
+     *
+     * @return User
+     */
+    public function setGroups($groups): User
+    {
+        $this->groups = $groups;
+
+        return $this;
     }
 }
--- harmony/user-bundle/master/src/Entity/User.php
+++ harmony/user-bundle/master/src/Entity/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Harmony\Bundle\UserBundle\Model\User as BaseUser;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
@@ -23,10 +24,51 @@ class User extends BaseUser
     protected $id;
 
     /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Group")
+     * @ORM\JoinTable(name="fos_user_user_group",
+     *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="group_id", referencedColumnName="id")}
+     * )
+     */
+    protected $groups;
+
+    /**
+     * User constructor.
+     */
+    public function __construct()
+    {
+        $this->groups = new ArrayCollection();
+    }
+
+    /**
      * @return int|string
      */
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Get Groups
+     *
+     * @return mixed
+     */
+    public function getGroups()
+    {
+        return $this->groups;
+    }
+
+    /**
+     * Set Groups
+     *
+     * @param mixed $groups
+     *
+     * @return User
+     */
+    public function setGroups($groups): User
+    {
+        $this->groups = $groups;
+
+        return $this;
     }
 }
